06fe36a71bc057e500d62704ecb78a33b862c4eb
arjune221105 <shakti221105@gmail.com>
2024-09-05 21:28:57 +0530

    one MajorChandes

---
+++ Airtel-Jio_Dummy/src/test/java/Dummy/Anand.java
@@ -0,0 +1,9 @@
+package Dummy;
+
+public class Anand {
+	public static void main(String[] args) {
+		System.out.println("Aanand this  is Dummy Project...");
+		System.out.println("Understood");
+	}
+
+}
